eb36dbf9385d89723c16eea3bff48895a8190c13
rhs <rhs@dev.io>
2023-01-27 09:54:33 +0900

    init

---
+++ src/test/java/io/dana/cote/january/Cote.java
@@ -0,0 +1,31 @@
+package io.dana.cote.january;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Cote {
+
+    Logger logger = LoggerFactory.getLogger(Cote.class);
+
+    /*
+    https://school.programmers.co.kr/learn/courses/30/lessons/150370
+     */
+    @Test
+    void 약관에_따른_개인정보_파기() {
+        //[1, 3]
+        String today = "2022.05.19";
+        String[] terms = {"A 6", "B 12", "C 3"};
+        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+    }
+
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public void caculateDate() {
+
+    }
+
+}
